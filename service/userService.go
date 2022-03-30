cc2869d5029a5c52db2be6bddfc833a9f6d8c4df
Revathi <revathi.thangavel@hotmail.com>
2022-03-30 20:51:59 +1100

    unit price update

--- service/userService.go
+++ service/userService.go
@@ -3,9 +3,9 @@ package service
 import (
 	"context"
 	"fmt"
+	"math"
 	"net/http"
 	"time"
-	"math"
 	//"github.com/go-chi/render"
 )
 
@@ -22,37 +22,33 @@ func (u *UserService) PerformCalculateProfit(ctx context.Context, w http.Respons
 	transactionData := req.TransactionData
 
 	/*render.JSON(w, r,
-		DataCalculateRevenue{
-			transactionData,
-			req.Config,
-		})
+	DataCalculateRevenue{
+		transactionData,
+		req.Config,
+	})
 
-*/
- processTransactions(transactionData,req.Config)
-
+	*/
+	processTransactions(transactionData, req.Config)
 
 }
 
-func processTransactions(transactions []Transaction,config Config){
+func processTransactions(transactions []Transaction, config Config) {
 	formatTransactions(&transactions)
-	fmt.Printf("%v+",transactions)
+	fmt.Printf("\n %v+", transactions)
 	/*buyShares := getbuyShares(transactions, config)
 	//fmt.Println(buyShares)
 	sellShares := getsellShares(transactions, config)
 	return calculatePandL(buyShares, sellShares, config)*/
 }
 
-func formatTransactions(transactions *[]Transaction){
-var date string
-var unitprice float32
-	for i,v := range *transactions{
-		
-		date = v.Date.Format("1/2/2006")
-		v.Date,_ = time.Parse("1/2/2006",date)
-		unitprice = v.Cost/float32(v.Quantity)
-		v.UnitPrice = float32(math.Abs(float64(unitprice)))
-		fmt.Printf("index: %v , value : %v \n",i,v)
+func formatTransactions(transactions *[]Transaction) {
+
+	for i := 0; i < len(*transactions); i++ {
+
+		unitprice := (*transactions)[i].Cost / float32((*transactions)[i].Quantity)
+
+		(*transactions)[i].UnitPrice = float32(math.Abs(float64(unitprice)))
+
 	}
-	
 
 }
